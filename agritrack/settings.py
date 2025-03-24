eb16438a9c9740997560e7ccc996a7d55b464de5
mcgarret254 <mcgarretronald@gmail.com>
2025-03-25 01:31:31 +0300

    fix

--- agritrack/settings.py
+++ agritrack/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY') or 'secret'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['agritrack-backend.onrender.com']
 
 
 AUTH_USER_MODEL = "users.User"
